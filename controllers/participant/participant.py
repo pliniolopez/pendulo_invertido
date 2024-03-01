4831394d01a966960beb8deb00084963d5835d9b
unknown <p4632@ulusofona.pt>
2024-03-01 02:18:20 +0000

    valor inicial kP

--- controllers/participant/participant.py
+++ controllers/participant/participant.py
@@ -23,7 +23,7 @@ rightMotor.setVelocity(0.0)
 maxSpeed = min(rightMotor.getMaxVelocity(), leftMotor.getMaxVelocity())
 
 # Define the PID control constants and variables.
-KP = 31.4
+KP = 60.4
 KI = 100.5
 KD = 0
 integral = 0.0
